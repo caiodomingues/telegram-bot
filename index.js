49d243c0961c455cb63749a57ecedb4511e63993
caiodomingues <caiodss99@hotmail.com>
2020-09-15 23:29:51 -0300

    feat: telegram bot

---
+++ index.js
@@ -0,0 +1,21 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "1213303184:AAHxGllAzwp3Jeeg9IPGbWIiqwO_DKDZMrY";
+const bot = new TelegramBot(token, { polling: true });
+
+let chatId;
+
+bot.on("message", (msg) => {
+  chatId = msg.chat.id;
+});
+
+bot.on("new_chat_members", async (ctx) => {
+  if (!ctx.new_chat_members) return;
+
+  if (ctx.new_chat_members.some((user) => !user.is_bot)) {
+    await bot.sendMessage(chatId, "MANDA");
+    await bot.sendMessage(chatId, "FOTO");
+    await bot.sendMessage(chatId, "DO");
+    await bot.sendMessage(chatId, "PÉ");
+  }
+});
